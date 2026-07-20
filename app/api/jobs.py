faa58b7dd6a8b48115ce621b14407ed10d898154
shriramrajat <rvshriram@dpcoepune.edu.in>
2026-07-20 20:55:44 +0530

    implement job scheduling system with Postgres storage, Redis queuing, and status management

--- app/api/jobs.py
+++ app/api/jobs.py
@@ -7,24 +7,30 @@ from app.core.redis import redis_client
 
 router = APIRouter(prefix="/jobs", tags=["Jobs"])
 
+from datetime import datetime, timezone
+from app.models.job import JobStatus
+
 @router.post("/", response_model=JobResponse, status_code=status.HTTP_202_ACCEPTED)
 def create_job(job_in: JobCreate, db: Session = Depends(get_db)):
-    # 1. Write to Postgres (The permanent record)
+    is_future = job_in.execute_at and job_in.execute_at > datetime.now(timezone.utc)
+    
+    # 1. Write to Postgres
     new_job = Job(
         task_name=job_in.task_name,
         payload=job_in.payload,
-        priority=job_in.priority
+        priority=job_in.priority,
+        execute_at=job_in.execute_at,
+        status=JobStatus.SCHEDULED if is_future else JobStatus.QUEUED
     )
     db.add(new_job)
     db.commit()
     db.refresh(new_job)
     
-    # 2. Push to Redis queue based on priority (e.g., 'queue:high')
-    queue_name = f"queue:{new_job.priority.value}"
-    
-    # We push just the Job ID. The worker fetches details from Postgres.
-    redis_client.lpush(queue_name, str(new_job.id))
-    
+    # 2. Push to Redis only if it's not a future job
+    if not is_future:
+        queue_name = f"queue:{new_job.priority.value}"
+        redis_client.lpush(queue_name, str(new_job.id))
+        
     # 3. Return instantly
     return new_job
 
